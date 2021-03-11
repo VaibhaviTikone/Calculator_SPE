c799b211eb0400237b48e05af1b6f21fef584def
vaibhavitikone <vaibhavi.tikone@iiitb.org>
2021-03-11 13:38:52 +0530

    New files are added

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,155 @@
+import java.util.Scanner;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import  java.util.InputMismatchException;
+
+public class Calculator {
+    private static final Scanner scanner = new Scanner(System.in);
+    private static final Logger logging = LogManager.getLogger(Calculator.class);
+
+    public Calculator(){}
+
+    public static void main(String[] args)
+    {
+        Calculator calc = new Calculator();
+        int choice;
+        System.out.println("\n\n\n\n----------------------------------------------------------------------------------------------");
+        System.out.println("\t\t\t\t\t\t\t\tWelcome to scietific calculator");
+        do{
+            try{
+                System.out.println("\n1. Square root \n2. Factorial\n3. Natural log\n4. Power");
+                System.out.print("\nEnter your choice: ");
+                choice = scanner.nextInt();
+            }
+            catch (InputMismatchException im)
+            {
+                logging.error("[ERROR]: Expecting integer value encountered different value type. ");
+                return;
+            }
+            switch(choice)
+            {
+                case 1: //square root
+                    double sqrt;
+                    try
+                    {
+                        System.out.println("Enter value: ");
+                        sqrt = scanner.nextDouble();
+                    }
+                    catch (InputMismatchException im)
+                    {
+                        logging.error("[ERROR]: Expecting double value encountered different value type. ");
+                        return;
+                    }
+                    System.out.println("Square root of " + sqrt + ": " + calc.Sqrt(sqrt));
+                    System.out.println("======================================================");
+                    break;
+                case 2: //Factorial code
+                    double fact;
+                    System.out.print("Enter the value: ");
+                    try{
+                        fact = scanner.nextInt();
+                    }
+                    catch (InputMismatchException im)
+                    {
+                        logging.error("[ERROR]: Expecting integer value encountered different value type. ");
+                        return;
+                    }
+                    double result;
+                    if(fact >=0){
+                        result = calc.Factorial(fact);
+                        System.out.println("Factorial of " + fact + " = " + result);
+                        System.out.println("======================================================");
+                    }
+                    else{
+                        logging.error("[ERROR]: Factorial is not defined for Negative numbers");
+                        return;
+                    }
+                    break;
+
+                case 3: // Natural log
+                    double logVal;
+                    try {
+                        System.out.print("Enter value: ");
+                        logVal= scanner.nextDouble();
+
+                    }
+                    catch (InputMismatchException ime)
+                    {
+                        logging.error("[ERROR]: Expected Double but got different type. Try again!!");
+                        return;
+                    }
+                    if(logVal>0) {
+                        System.out.println("Log of " + logVal + ": " + calc.Log(logVal));
+                        System.out.println("======================================================");
+                    }
+                    else{
+                        logging.error("[ERROR]: Log is not defined for Negative numbers");
+                        return;
+                    }
+                    break;
+                case 4: // power function
+                    double base,exp;
+
+                    try {
+                        System.out.print("Enter base value: ");
+                        base = scanner.nextDouble();
+                        System.out.print("Enter exponent value: ");
+                        exp = scanner.nextDouble();
+
+                    }
+                    catch (InputMismatchException ime) {
+                        logging.error("[ERROR]: Value type mismatch.Expected Double got other type. ");
+                        return ;
+                    }
+                    System.out.println(base + " raised to " + exp +": "+calc.Power(base,exp));
+                    System.out.println("======================================================");
+
+                    break;
+                default:
+                    logging.info("[INFO]: Exiting, entered choice doesn't have any functionality.");
+            }
+        }while(choice<=4);
+        System.out.println("\n\n\t\t\t\t\t\t\t\t\t\t\t You are exiting");
+        System.out.println("----------------------------------------------------------------------------------------------");
+    }
+
+    public double Sqrt(double sqrt)
+    {
+
+        logging.info("[INFO]: value: " + sqrt);
+        if(sqrt<0)
+        {
+            logging.error("[ERROR]: square root is not defined for negative numbers");
+            return Double.NaN;
+        }
+        return Math.sqrt(sqrt);
+    }
+
+
+    public double Factorial(double n)
+    {
+        logging.info("[INFO]: I/P for factorial: " + n);
+        if(n<0) return Double.NaN;
+        double factorial=1;
+        for(int i = 2; i <= n; ++i)
+        {
+
+            factorial*=i;
+        }
+        logging.info("[INFO]: Factorial result: " + factorial);
+        return factorial;
+    }
+
+    public double Log(double logVal)
+    {
+        logging.info("[INFO]: Value: "+logVal);
+        return Math.log(logVal);
+    }
+    public double Power(double base, double exp)
+    {
+        logging.info("[INFO]: Base: "+base +" Exponent: "+exp);
+        if(base==0 && exp ==0)
+            return Double.NaN;
+        return Math.pow(base,exp);
+    }
+}
---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,83 @@
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class CalculatorTest {
+
+    private static final double DELTA = 1e-10;
+    private static Calculator calc;
+
+    @BeforeClass
+    public static void initialize(){
+        calc = new Calculator();
+
+    }
+
+    @Test
+    public  void  factorialPositive()
+    {
+        assertEquals("[Positive Case]: Factorial of a number, ",120,calc.Factorial(5),DELTA);
+        assertEquals("[Positive Case]: Factorial of a number, ",5040,calc.Factorial(7),DELTA);
+        assertEquals("[Positive Case]: Factorial of a number, ",Double.NaN,calc.Factorial(-2),DELTA);
+    }
+
+    @Test
+    public void factorialNegative()
+    {
+        assertNotEquals("[Negative Case]: Factorial of a number,",24,calc.Factorial(5),DELTA);
+        assertNotEquals("[Negative Case]: Factorial of a number,",120,calc.Factorial(7),DELTA);
+        assertNotEquals("[Negative Case]: Factorial of a number,",5,calc.Factorial(-3),DELTA);
+    }
+
+    @Test
+    public  void  exponentiationPositive()
+    {
+        assertEquals("[Positive Case]: Power of a number, ",32,calc.Power(2,5),DELTA);
+        assertEquals("[Positive Case]: Power of a number, ",0.1111111111,calc.Power(3,-2),DELTA);
+        assertEquals("[Positive Case]: Power of a number, ",Double.NaN,calc.Power(0,0),DELTA);
+    }
+
+
+    @Test
+    public  void  exponentiationNegative()
+    {
+        assertNotEquals("[Negative Case]: Power of a Number, ",31,calc.Power(2,5),DELTA) ;
+        assertNotEquals("[Negative Case]: Power of a Number, ",0.1111111122,calc.Power(3,-2),DELTA);
+        assertNotEquals("[Negative Case]: Power of a Number, ",1,calc.Power(0,0),DELTA);
+    }
+
+    @Test
+    public  void  logPositive()
+    {
+        assertEquals("[Positive Case]: Log of a number, ",2.302585092994,calc.Log(10),DELTA);
+        assertEquals("[Positive Case]: Log of a number, ",Double.NEGATIVE_INFINITY,calc.Log(0),DELTA);
+        assertEquals("[Positive Case]: Log of a number, ",Double.NaN,calc.Log(-5),DELTA);
+    }
+
+
+    @Test
+    public  void  logNegative()
+    {
+        assertNotEquals("[Negative Case]: Log of a number, ", 6, calc.Log(3), DELTA);
+        assertNotEquals("[Negative Case]: Log of a number, ", -7.3, calc.Log(1.6), DELTA);
+        assertNotEquals("[Negative Case]: Log of a number, ", 0, calc.Log(0), DELTA);
+        assertNotEquals("[Negative Case]: Log of a number, ", 9.0, calc.Log(-5), DELTA);
+    }
+
+
+    @Test
+    public void sqrtPositive(){
+        assertEquals("[Positive Case]: SQRT of a number, ", 2, calc.Sqrt(4), DELTA);
+        assertEquals("[Positive Case]: SQRT of a number, ", 2.236067977499, calc.Sqrt(5), DELTA);
+        assertEquals("[Positive Case]: SQRT of a number, ", 0, calc.Sqrt(0), DELTA);
+        assertEquals("[Positive Case]: SQRT of a number, ", Double.NaN, calc.Sqrt(-2), DELTA);
+    }
+
+    @Test
+    public void sqrtNegative(){
+        assertNotEquals("[Negative Case]: SQRT of a number, ", 6, calc.Sqrt(4), DELTA);
+        assertNotEquals("[Negative Case]: SQRT of a number, ", 4.2, calc.Sqrt(2.1), DELTA);
+        assertNotEquals("[Negative Case]: SQRT of a number, ", 7.3, calc.Sqrt(-5), DELTA);
+    }
+
+}
